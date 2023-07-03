028c0d352bba40271ff568175c853d64ccfda1a4
junh0328 <junh0328@naver.com>
2023-07-03 18:26:34 +0900

    [remove] Remove unnecessary file

--- src/lib/gtag/events.ts
+++
@@ -1,8 +0,0 @@
-export const sendIsLoggedInEvent = (url: string, isAuth: boolean) => {
-    return {
-        action: '[CLICK] BUTTON',
-        category: `[NAVIGATE] to ${url}`,
-        label: 'isLoggedIn',
-        value: `${isAuth}`,
-    }
-}
