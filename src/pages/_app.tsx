3af268902e8c1a03cb39dadd0dab8e4a5367cfdb
junh0328 <junh0328@naver.com>
2023-07-03 17:46:44 +0900

    [chore] Change the GA4 strategies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,44 @@
 import Header from "@/components/Header";
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import * as gtag from "../lib/gtag";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+
+      <Script
+        id="ga4_init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}');
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname
+              });
             `,
-          }}
-        />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        ></script>
-      </Head>
+        }}
+      />
 
       <Header />
       <Component {...pageProps} />
