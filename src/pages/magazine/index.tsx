6a31a286b8530ea3df59b0205fa89a48843848ca
junhee <junhee@eazel.net>
2023-07-04 12:09:28 +0900

    [feat] Bind gtag event on click event

--- src/pages/magazine/index.tsx
+++ src/pages/magazine/index.tsx
@@ -1,18 +1,29 @@
-import HeadMeta from "@/components/HeadMeta";
+import HeadMeta from '@/components/HeadMeta';
+import * as gtag from '@/lib/gtag';
 
 const Magazine = () => {
+  const handleClick = () => {
+    gtag.event({
+      action: 'click',
+      category: 'button',
+      label: 'click_magazine',
+      value: '1',
+    });
+  };
   return (
     <>
       <HeadMeta
         description="junhee's magazine"
-        title="junhee | magazine"
-        image=""
-        url="https://ga-4.vercel.app/magazine"
+        title='junhee | magazine'
+        image=''
+        url='https://ga-4.vercel.app/magazine'
       />
 
       <div>
         <h1>Hello Magazine</h1>
       </div>
+
+      <button onClick={handleClick}>CLICK MAGAZINE</button>
     </>
   );
 };
