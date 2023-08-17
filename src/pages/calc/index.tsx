f319f0eefd6d872ef0ea9bd3d4b65cb21d342f14
junhee <junhee@eazel.net>
2023-08-17 10:16:01 +0900

    [chore] add modal

--- src/pages/calc/index.tsx
+++ src/pages/calc/index.tsx
@@ -1,10 +1,23 @@
 import { styled } from '@stitches/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Calc = () => {
+  const [isModal, setIsModal] = useState(false);
   return (
     <Container>
       <h1>Hello Container</h1>
+
+      <button onClick={() => setIsModal(true)}>Modal</button>
+
+      {isModal && (
+        <Modal>
+          <button onClick={() => setIsModal(false)}>X</button>
+
+          <div>
+            <h1>Hello Modal</h1>
+          </div>
+        </Modal>
+      )}
     </Container>
   );
 };
@@ -13,6 +26,16 @@ export default Calc;
 
 const Container = styled('div', {
   width: '100%',
-  background: 'AntiqueWhite',
-  height: `calc(var(--vh, 1vh) * 90)`, // Fixed here
+  background: 'Aqua',
+  height: `calc(var(--vh, 1vh) * 90)`,
+});
+
+const Modal = styled('div', {
+  width: '100%',
+  height: `calc(var(--vh, 1vh) * 100)`,
+  backdropFilter: 'blur(150px)',
+  zIndex: 100,
+  position: 'absolute',
+  top: 0,
+  left: 0,
 });
